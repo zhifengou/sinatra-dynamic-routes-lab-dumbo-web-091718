43d0b579c940d1e5fc7fb5e66ddf27dc01b8c311
zhifeng ou <zhifengou@hotmail.com>
2018-10-08 21:53:33 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,5 +2,21 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
-
+  get '/reversename/:name' do 
+    
+  end 
+  
+  get '/square/:number' do 
+    
+  end 
+  
+  get '/say/:number/:phrase' do 
+  
+   end 
+   
+   get '/say/:word1/:word2/:word3/:word4/:word5' do 
+    
+  end 
+  
+  get '/:operation/:number1/:number2' 
 end
