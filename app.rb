e464ffd6fb7534bf137141db4dc1db82b148f770
zhifeng ou <zhifengou@hotmail.com>
2018-10-08 22:19:57 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -19,10 +19,8 @@ class App < Sinatra::Base
   end 
    
    get '/say/:word1/:word2/:word3/:word4/:word5' do 
-    # "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
-    str=''
-    str=params[:word1]+params[:word2]+params[:word3]+params[:word4]+params[:word5]+'.'
-    str
+    "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
+   
   end 
   
   get '/:operation/:number1/:number2' do 
