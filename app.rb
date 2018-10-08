c47c7f64fdaf1f73868eb0846b9897afd21f3267
zhifeng ou <zhifengou@hotmail.com>
2018-10-08 22:21:41 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -23,6 +23,8 @@ class App < Sinatra::Base
   end 
   
   get '/:operation/:number1/:number2' do 
-    
+    if params[:operation]== "add"
+      params[:number1]+params[:number2]
+    end
   end
 end
