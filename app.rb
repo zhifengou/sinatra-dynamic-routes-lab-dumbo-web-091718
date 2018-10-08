a4bfc0972a06d67bee173891a1b1c49d6af5906d
zhifeng ou <zhifengou@hotmail.com>
2018-10-08 22:07:42 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -12,11 +12,10 @@ class App < Sinatra::Base
   end 
   
   get '/say/:number/:phrase' do 
-    @number = params[:number].to_i 
-    @phrase = params[:phrase]
-    @number.times do 
-      @phrase
-    end 
+   
+      params[:number].to_i.times do
+        params[:phrase]
+    end
   end 
    
    get '/say/:word1/:word2/:word3/:word4/:word5' do 
