0e99fdc58f26f0f0bda404d11bc8610e22823daf
zhifeng ou <zhifengou@hotmail.com>
2018-10-08 21:55:09 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   # Write your code here!
   get '/reversename/:name' do 
-    @name = 
+    @name = params[:name].reverse 
   end 
   
   get '/square/:number' do 
