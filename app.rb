9a99bf919f37e9dcb36cb8a17a8cc3b1648f8fb3
zhifeng ou <zhifengou@hotmail.com>
2018-10-08 22:26:07 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -30,6 +30,7 @@ class App < Sinatra::Base
     elsif params[:operation] == "multiply"
       (params[:number1].to_i)*(params[:number2].to_i).to_s
     elsif params[:operation] == "divide"
+      (params[:number1].to_i)/(params[:number2].to_i).to_s
     end
   end
 end
